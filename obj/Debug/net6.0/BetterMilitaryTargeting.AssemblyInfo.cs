1e69911095e8c6e5342cd388b6ddb7d8609ef131
Jonyboylovespie <jonathan.frawley@outlook.com>
2025-03-04 19:18:54 -0500

    Update

--- obj/Debug/net6.0/BetterMilitaryTargeting.AssemblyInfo.cs
+++ obj/Debug/net6.0/BetterMilitaryTargeting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BetterMilitaryTargeting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5868c34d29d8f317edc9d9318b9ae6bb9491f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BetterMilitaryTargeting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BetterMilitaryTargeting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
